460186f1227e5a66f774af53495b891508eb6966
Koihcire <ericchiok@gmail.com>
2022-08-27 09:48:39 +0000

    test logout

--- middlewares/index.js
+++ middlewares/index.js
@@ -12,22 +12,25 @@ const checkIfAuthenticated = function (req,res,next){
 
 const checkIfAuthenticatedJWT = function (req,res,next){
     const authHeader = req.headers.authorization;
+    // console.log(authHeader)
     if(authHeader){
         //extract out the jwt and check if valid
         //example header authHeader => BEARER kjnAKJNinalknKJANSFsd
         const token = authHeader.split(' ')[1]; //split by space, take the index 1 which is jwt
-        // console.log(token)
+        // console.log('token=>', token)
         jwt.verify(token, process.env.TOKEN_SECRET, function(err,tokenData){
             // err argument is null if there is no error
             //token data argument is the data we embed
             if(err){
                 res.status(401);
+                // console.log("checkfailed")
                 res.json({
                     'error': 'Invalid access token'
                 })
             } else {
                 // if token is valid
                 req.customer = tokenData;
+                // console.log("checkifauthsuccess")
                 next();
             }
         })
