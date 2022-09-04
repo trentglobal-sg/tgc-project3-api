4fd7f2c6508fb3eb4a00d72ec11c36185a8894a2
Koihcire <ericchiok@gmail.com>
2022-09-04 08:58:15 +0000

    relocated login to landing page by default

--- routes/landing.js
+++ routes/landing.js
@@ -1,10 +1,55 @@
 const express = require("express");
-
+const {createLoginForm, createUserForm, bootstrapField} = require('../forms')
 //create a router object, can contain routes
 const router = express.Router();
+const {getHashedPassword} = require('../utilities')
+const {User} = require('../models')
 
 router.get('/', function(req,res){
-    res.render("landing/index");
+    const loginForm = createLoginForm()
+
+    res.render("landing/index",{
+        form: loginForm.toHTML(bootstrapField)
+    });
+})
+
+router.post('/', (req,res)=>{
+    const loginForm = createLoginForm();
+
+    loginForm.handle(req,{
+        'success': async (form) => {
+            //process login
+            // ...find the user by email and password
+            const user = await User.where({
+                'username': form.data.username,
+                'password': getHashedPassword(form.data.password)
+            }).fetch({
+                require: false
+            });
+
+            //check if the user does not exist
+            if (!user) {
+                req.flash("error_messages", "Sorry, wrong username or password.")
+                res.redirect('/');
+            } else {
+                // store the user details
+                req.session.user = {
+                    id: user.get('id'),
+                    username: user.get('username'),
+                    email: user.get('email'),
+                    role_id: user.get('role_id')
+                }
+                req.flash("success_messages", "Welcome back, " + user.get('username'));
+                res.redirect('/products');
+            }
+        },
+        'error': async (form) => {
+            req.flash("error_messages", "There are some problems logging you in. Please fill in the form again")
+            res.render('/', {
+                'form': form.toHTML(bootstrapField)
+            })
+        },
+    })
 })
 
 router.get('/about-us', function(req,res){
--- routes/users.js
+++ routes/users.js
@@ -50,57 +50,58 @@ router.post('/register', (req,res)=>{
     })
 })
 
-router.get('/login', (req,res)=>{
-    const loginForm = createLoginForm()
+// MOVED LOGIN ROUTE TO LANDING / INDEX
+// router.get('/login', (req,res)=>{
+//     const loginForm = createLoginForm()
 
-    res.render('users/login', {
-        'form': loginForm.toHTML(bootstrapField)
-    })
-})
+//     res.render('users/login', {
+//         'form': loginForm.toHTML(bootstrapField)
+//     })
+// })
 
-router.post('/login', (req,res)=>{
-    const loginForm = createLoginForm();
+// router.post('/login', (req,res)=>{
+//     const loginForm = createLoginForm();
 
-    loginForm.handle(req,{
-        'success': async (form) => {
-            //process login
-            // ...find the user by email and password
-            const user = await User.where({
-                'username': form.data.username,
-                'password': getHashedPassword(form.data.password)
-            }).fetch({
-                require: false
-            });
+//     loginForm.handle(req,{
+//         'success': async (form) => {
+//             //process login
+//             // ...find the user by email and password
+//             const user = await User.where({
+//                 'username': form.data.username,
+//                 'password': getHashedPassword(form.data.password)
+//             }).fetch({
+//                 require: false
+//             });
 
-            //check if the user does not exist
-            if (!user) {
-                req.flash("error_messages", "Sorry, wrong username or password.")
-                res.redirect('/users/login');
-            } else {
-                // store the user details
-                req.session.user = {
-                    id: user.get('id'),
-                    username: user.get('username'),
-                    email: user.get('email'),
-                    role_id: user.get('role_id')
-                }
-                req.flash("success_messages", "Welcome back, " + user.get('username'));
-                res.redirect('/products');
-            }
-        },
-        'error': async (form) => {
-            req.flash("error_messages", "There are some problems logging you in. Please fill in the form again")
-            res.render('users/login', {
-                'form': form.toHTML(bootstrapField)
-            })
-        },
-    })
-})
+//             //check if the user does not exist
+//             if (!user) {
+//                 req.flash("error_messages", "Sorry, wrong username or password.")
+//                 res.redirect('/');
+//             } else {
+//                 // store the user details
+//                 req.session.user = {
+//                     id: user.get('id'),
+//                     username: user.get('username'),
+//                     email: user.get('email'),
+//                     role_id: user.get('role_id')
+//                 }
+//                 req.flash("success_messages", "Welcome back, " + user.get('username'));
+//                 res.redirect('/products');
+//             }
+//         },
+//         'error': async (form) => {
+//             req.flash("error_messages", "There are some problems logging you in. Please fill in the form again")
+//             res.render('/', {
+//                 'form': form.toHTML(bootstrapField)
+//             })
+//         },
+//     })
+// })
 
 router.get('/logout', (req, res) => {
     req.session.user = null;
     req.flash('success_messages', "Goodbye");
-    res.redirect('/users/login');
+    res.redirect('/');
 })
 
 router.get('/register',checkIfAuthenticated, (req,res)=>{
